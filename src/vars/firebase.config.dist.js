a33f24236f90965f3bf0a3a132a883405bcdb3b0
gkueny <gaetankueny@gmail.com>
2017-01-17 15:57:35 +0100

    add firebase config dist

---
+++ src/vars/firebase.config.dist.js
@@ -0,0 +1,7 @@
+export const config = {
+    apiKey: "your_api_key",
+    authDomain: "name_app.firebaseapp.com",
+    databaseURL: "https://name_app.firebaseio.com",
+    storageBucket: "name_app.appspot.com",
+    messagingSenderId: "your_message_sender_id"
+};
